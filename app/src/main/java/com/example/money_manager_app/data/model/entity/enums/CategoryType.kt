f3a8aa4fe20e256c5334cb9a3e9f9a81683fd4ea
Hoang Pham <90328064+Hanogi610@users.noreply.github.com>
2024-12-01 14:41:12 +0700

    update category entity

--- app/src/main/java/com/example/money_manager_app/data/model/entity/enums/CategoryType.kt
+++ app/src/main/java/com/example/money_manager_app/data/model/entity/enums/CategoryType.kt
@@ -1,19 +1,5 @@
 package com.example.money_manager_app.data.model.entity.enums
 
 enum class CategoryType {
-    ALL_CATEGORIES,
-    BILLS,
-    CLOTHING,
-    EDUCATION,
-    ENTERTAINMENT,
-    FITNESS,
-    FOOD,
-    GIFTS,
-    HEALTH,
-    FURNITURE,
-    PET,
-    SHOPPING,
-    TRANSPORTATION,
-    TRAVEL,
-    OTHERS
+    INCOME, EXPENSE
 }
